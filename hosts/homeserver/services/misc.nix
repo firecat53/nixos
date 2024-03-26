3d8244d604f546bbe68442ac9612898e2ee1684d
Scott Hansen <tech@firecat53.net>
2024-03-25 21:55:10 -0700

    Use exiftool directly instead of pix.py

--- hosts/homeserver/services/misc.nix
+++ hosts/homeserver/services/misc.nix
@@ -27,15 +27,20 @@
     wantedBy = ["timers.target"];
     timerConfig = {
       OnBootSec = "5m";
-      OnCalendar = "daily";
+      OnUnitActiveSec = "3h";
       Unit = "picture_copy.service";
     };
   };
   systemd.services."picture_copy" = {
-    script = "${pkgs.python3}/bin/python3 /home/firecat53/docs/family/scott/src/scripts.git/bin/pix.py";
-    path = [pkgs.python3 pkgs.rsync pkgs.exiftool];
+    script = ''
+      ${pkgs.exiftool}/bin/exiftool -o . '-Directory<DateTimeOriginal' '-Directory<MediaCreateDate' '-Directory<TimeStamp' -d "/mnt/media/pictures/Family Pictures/%Y/%Y - %B/" -ext jpg -ext png -ext heic -ext tif -ext jpeg -r -P /mnt/media/cameras/ 2>/dev/null
+      ${pkgs.exiftool}/bin/exiftool -o . '-Directory<DateTimeOriginal' '-Directory<MediaCreateDate' -d "/mnt/media/video/Family/%Y/%Y - %B/" -ext mp4 -ext avi -ext 3gp -r -P /mnt/media/cameras/ 2>/dev/null
+    '';
+    path = [pkgs.exiftool];
     serviceConfig = {
       User = "firecat53";
+      SuccessExitStatus = [ 1 ]; # Exiftool errors on file exists when it tries to copy
+      WorkingDirectory = "/home/firecat53/.local/tmp/untagged";
     };
   };
 }
