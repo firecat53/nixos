5e352bc1a6193a5c45550c6847f34352ec67710a
Scott Hansen <tech@firecat53.net>
2024-09-12 08:35:44 -0700

    Fix bug in picture_copy. Allows rest of the script to run

--- hosts/homeserver/services/misc.nix
+++ hosts/homeserver/services/misc.nix
@@ -33,6 +33,7 @@
   };
   systemd.services."picture_copy" = {
     script = ''
+      set +e
       ${pkgs.exiftool}/bin/exiftool -o . '-Directory<DateTimeOriginal' '-Directory<MediaCreateDate' '-Directory<TimeStamp' -d "/mnt/media/pictures/Family Pictures/%Y/%Y - %B/" -ext jpg -ext png -ext heic -ext tif -ext jpeg -r -P /mnt/media/cameras/ 2>/dev/null
       ${pkgs.exiftool}/bin/exiftool -o . '-Directory<DateTimeOriginal' '-Directory<MediaCreateDate' -d "/mnt/media/video/Family/%Y/%Y - %B/" -ext mp4 -ext avi -ext 3gp -r -P /mnt/media/cameras/ 2>/dev/null
     '';
