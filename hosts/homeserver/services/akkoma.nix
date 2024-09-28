3fddaed883b42612da0b7350673e1d5511579f56
Scott Hansen <tech@firecat53.net>
2024-09-28 09:14:41 -0700

    Use newer ffmpeg for akkoma and redirect firecat53.net to nginx. Strip exif data from uploads.

--- hosts/homeserver/services/akkoma.nix
+++ hosts/homeserver/services/akkoma.nix
@@ -22,8 +22,16 @@
         "Pleroma.Web.WebFinger" = {
           domain = "firecat53.net";
         };
+        # Strips GPS, deduplicates and anonymizes filenames for uploaded files
+        "Pleroma.Upload".filters = map (pkgs.formats.elixirConf { }).lib.mkRaw [
+          "Pleroma.Upload.Filter.Exiftool"
+          "Pleroma.Upload.Filter.Dedupe"
+          "Pleroma.Upload.Filter.AnonymizeFilename"
+        ];
       };
     };
+    # use default ffmpeg instead of the module default ffmpeg_5
+    extraPackages = with pkgs; [ exiftool ffmpeg-headless graphicsmagick-imagemagick-compat ];
   };
   services.traefik.dynamicConfigOptions.http = {
     middlewares.well-known-redirect.redirectRegex = {
@@ -54,7 +62,11 @@
         }
       ];
 
-      dummy-well-known.loadBalancer.servers = [ {} ];
+      dummy-well-known.loadBalancer.servers = [
+        {
+          # Redirect to nginx
+          url = "http://localhost:${builtins.toString config.services.nginx.defaultHTTPListenPort}";
+        } ];
     };
   };
 }
