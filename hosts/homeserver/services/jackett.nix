0b9bd0248cea3c91da0d9a7d263d7f415ff04daa
Scott Hansen <tech@firecat53.net>
2025-01-15 10:28:14 -0800

    Disable tests for jackett that fail routinely

--- hosts/homeserver/services/jackett.nix
+++ hosts/homeserver/services/jackett.nix
@@ -10,6 +10,12 @@
     group = "users";
     dataDir = "/var/lib/jackett";
   };
+  # Disable failing tests (2025-01-15)
+  nixpkgs.overlays = [ 
+    (final: prev: { 
+      jackett = prev.jackett.overrideAttrs { doCheck = false; }; 
+    })
+  ];
   services.traefik.dynamicConfigOptions.http.routers.jackett = {
     rule = "Host(`jackett.lan.firecat53.net`)";
     service = "jackett";
