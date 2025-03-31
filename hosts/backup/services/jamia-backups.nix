2acf5625adbdcde2835fe2129f7c8933bf9c1f34
Scott Hansen <tech@firecat53.net>
2025-03-31 11:09:50 -0700

    Remove samba from backup server

--- hosts/backup/services/jamia-backups.nix
+++ hosts/backup/services/jamia-backups.nix
@@ -14,43 +14,4 @@
   environment.systemPackages = with pkgs; [
     kopia
   ];
-
-  # For Samba mount
-  fileSystems = {
-    "/home/jamia/backups" = {
-      device = "backuppool/jamia";
-      fsType = "zfs";
-      options = [ "X-mount.mkdir" ];
-    };
-  };
-
-  systemd.tmpfiles.rules = [
-    "d /var/lib/samba/private 0755 root root - "
-    "d /var/log/samba/ 0755 root root - "
-  ];
-
-  services.samba = {
-    enable = true;
-    openFirewall = true;
-    settings = {
-      global = {
-        workgroup = "HOME";
-        "server string" = "backup";
-        "netbios name" = "backup";
-        "hosts allow" = "192.168.200. 10.200.200. 127.0.0.1 localhost";
-        "hosts deny" = "0.0.0.0/0";
-        "guest account" = "nobody";
-        "map to guest" = "bad user";
-        security = "user";
-      };
-      backups = {
-        comment = "Backup directory";
-        path = "/home/jamia/backups";
-        "valid users" = "jamia";
-        browseable = "yes";
-        writeable = "yes";
-        "read only" = "no";
-      };
-    };
-  };
 }
