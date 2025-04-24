a6eac9703806da0215dae2e79cc79d1e4e78ae39
Scott Hansen <tech@firecat53.net>
2025-04-24 09:27:29 -0700

    Remove flake-update service.
    When flake.lock is not in git, nixos-rebuild updates the lock file each
    time it runs.

--- hosts/modules/common/nix.nix
+++ hosts/modules/common/nix.nix
@@ -94,8 +94,6 @@ in
       StartLimitIntervalSec = 600;
       StartLimitBurst = 2;
     };
-    after = [ "flake-update.service" ];
-    wants = [ "flake-update.service" ];
     path = [ pkgs.host ];
   };
   nix.gc = {
@@ -108,29 +106,5 @@ in
   ## Show installed packages
   environment.shellAliases = {
     ni = "nix-store --query --requisites /run/current-system/sw | cut -d- -f2- | sort | less";
-  };
-  ## Update flake inputs daily
-  systemd.services = {
-    flake-update = {
-      preStart = "${pkgs.host}/bin/host firecat53.net"; # Check network connectivity
-      unitConfig = {
-        Description = "Update flake inputs";
-        StartLimitIntervalSec = 300;
-        StartLimitBurst = 5;
-      };
-      serviceConfig = {
-        ExecStart = "${pkgs.nix}/bin/nix flake update --flake ${flakePath}";
-        Restart = "on-failure";
-        RestartSec = "30";
-        Type = "oneshot"; # Ensure that it finishes before starting nixos-upgrade
-        User = "${user}";
-      };
-      before = [ "nixos-upgrade.service" ];
-      path = [
-        pkgs.nix
-        pkgs.git
-        pkgs.host
-      ];
-    };
   };
 }
