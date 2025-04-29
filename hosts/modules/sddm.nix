e86da5bc2ef7874cd1a046640e3ab59fc8a2219b
Scott Hansen <tech@firecat53.net>
2025-04-29 13:34:07 -0700

    SDDM update

--- hosts/modules/sddm.nix
+++ hosts/modules/sddm.nix
@@ -3,8 +3,20 @@
   ...
 }:
 {
-  services.displayManager = {
-    sddm.enable = true;
-    sddm.package = pkgs.kdePackages.sddm;
+  services.displayManager.sddm = {
+    enable = true;
+    enableHidpi = true;
+    extraPackages = [ pkgs.catppuccin-sddm ];
+    package = pkgs.kdePackages.sddm;
+    theme = "catppuccin-mocha";
+    wayland.enable = true;
   };
+  environment.systemPackages = [
+    (pkgs.catppuccin-sddm.override {
+      flavor = "mocha";
+      font = "DevjaVu Sans";
+      fontSize = "17";
+      loginBackground = true;
+    })
+  ];
 }
