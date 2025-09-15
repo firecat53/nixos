066bc5c98c7023b21273ffd61edc910e9be76672
Scott Hansen <tech@firecat53.net>
2025-09-14 19:18:37 -0700

    Switch back to unstable for open-webui. Add workaround to fix initial admin user creation error

--- hosts/homeserver/services/openwebui.nix
+++ hosts/homeserver/services/openwebui.nix
@@ -1,13 +1,22 @@
 # Open-WebUI
 {
+  config,
   pkgs,
   ...
 }:
 {
   services.open-webui = {
-    package = pkgs.open-webui;
+    package = pkgs.unstable.open-webui;
     enable = true;
     port = 8083;
+    # per https://github.com/NixOS/nixpkgs/pull/431395#issuecomment-3161532401
+    # Fixes inability to setup initial user
+    environment = {
+      STATIC_DIR = "${config.services.open-webui.stateDir}/static";
+      DATA_DIR = "${config.services.open-webui.stateDir}/data";
+      HF_HOME = "${config.services.open-webui.stateDir}/hf_home";
+      SENTENCE_TRANSFORMERS_HOME = "${config.services.open-webui.stateDir}/transformers_home";
+    };
   };
   services.traefik.dynamicConfigOptions.http.routers.openwebui = {
     rule = "Host(`ai.lan.firecat53.net`)";
