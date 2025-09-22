f80e4609da981308bd716b0a1b90387a9826294e
Scott Hansen <tech@firecat53.net>
2025-09-22 15:47:49 -0700

    Include owner/group and user/hostname in yazi header/footer

--- home-manager/common/yazi.nix
+++ home-manager/common/yazi.nix
@@ -81,6 +81,25 @@
     };
     initLua = ''
       require("git"):setup()
+      Status:children_add(function()
+        local h = cx.active.current.hovered
+        if not h or ya.target_family() ~= "unix" then
+          return ""
+        end
+
+        return ui.Line {
+          ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+          ":",
+          ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+          " ",
+        }
+      end, 500, Status.RIGHT)
+      Header:children_add(function()
+        if ya.target_family() ~= "unix" then
+          return ""
+        end
+        return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+      end, 500, Header.LEFT)
     '';
   };
 }
