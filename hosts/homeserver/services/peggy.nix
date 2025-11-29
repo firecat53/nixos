e92a88b35837f3e9b1e114baf2833c896877f66f
Scott Hansen <tech@firecat53.net>
2025-11-28 16:17:50 -0800

    Add bitwarden vault backup for Peggy

--- hosts/homeserver/services/peggy.nix
+++ hosts/homeserver/services/peggy.nix
@@ -1,5 +1,6 @@
 # Peggy camera backups
 {
+  config,
   pkgs,
   ...
 }:
@@ -30,6 +31,55 @@
       User = "firecat53";
       SuccessExitStatus = [ 1 ]; # Exiftool errors on file exists when it tries to copy
       WorkingDirectory = "/home/firecat53/.local/tmp/untagged-peggy";
+    };
+  };
+
+  # Automatic bitwarden vault backup to /var/backups/
+  sops.secrets.bitwarden-clientid-peggy = { };
+  sops.secrets.bitwarden-clientsecret-peggy = { };
+  sops.secrets.bitwarden-password-peggy = { };
+
+  systemd.services.bitwarden-backup-peggy = {
+    description = "Backup Bitwarden vault";
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+      LoadCredential = [
+        "client_id:${config.sops.secrets.bitwarden-clientid-peggy.path}"
+        "client_secret:${config.sops.secrets.bitwarden-clientsecret-peggy.path}"
+        "password:${config.sops.secrets.bitwarden-password-peggy.path}"
+      ];
+    };
+    script = ''
+      STATUS=$(${pkgs.bitwarden-cli}/bin/bw status | ${pkgs.jq}/bin/jq -r '.status')
+
+      if [ "$STATUS" = "unauthenticated" ]; then
+        export BW_CLIENTID=$(cat $CREDENTIALS_DIRECTORY/client_id)
+        export BW_CLIENTSECRET=$(cat $CREDENTIALS_DIRECTORY/client_secret)
+        ${pkgs.bitwarden-cli}/bin/bw login --apikey
+      elif [ "$STATUS" != "locked" ]; then
+        echo "Error: Unknown vault status: $STATUS"
+        exit 1
+      fi
+      export BW_SESSION=$(${pkgs.bitwarden-cli}/bin/bw unlock \
+        --raw \
+        --passwordfile=$CREDENTIALS_DIRECTORY/password)
+
+      ${pkgs.bitwarden-cli}/bin/bw sync
+      ${pkgs.bitwarden-cli}/bin/bw export \
+        --format json \
+        --output /var/backups/bitwarden-backup-peggy.json
+
+      ${pkgs.bitwarden-cli}/bin/bw lock
+    '';
+    path = [ pkgs.bitwarden-cli pkgs.jq ];
+  };
+  systemd.timers.bitwarden-backup-peggy = {
+    description = "Backup Peggy's Bitwarden vault daily";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
     };
   };
 }
