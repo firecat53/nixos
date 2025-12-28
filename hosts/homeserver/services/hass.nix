848b1305ac2ac8bd7e2e76f3e2c6f02235152eb5
Scott Hansen <tech@firecat53.net>
2025-12-28 07:44:17 -0800

    Switch from conbee to new SMLIGHT thread adapter for homeassistant

--- hosts/homeserver/services/hass.nix
+++ hosts/homeserver/services/hass.nix
@@ -23,17 +23,17 @@
     };
   };
 
-  # Create conbee.xml for Conbee II Thread device. Used to attach/reattach to the VM
-  environment.etc."conbee.xml" = {
+  # Create thread.xml for SMLIGHT SLZB-07 Thread device. Used to attach/reattach to the VM
+  environment.etc."thread.xml" = {
     text = ''
       <hostdev mode='subsystem' type='usb' managed='yes'>
           <source>
-              <vendor id='0x1cf1'/>
-              <product id='0x0030'/>
+              <vendor id='0x10c4'/>
+              <product id='0xea60'/>
           </source>
       </hostdev>
     '';
-    target = "homeassistant/conbee.xml";
+    target = "homeassistant/thread.xml";
   };
   # Create zbt2.xml for Zigbee ZBT-2 device. Used to attach/reattach to the VM
   environment.etc."zbt2.xml" = {
@@ -62,10 +62,10 @@
         # Wait for VM to fully start
         ${pkgs.coreutils}/bin/sleep 30
         if ${pkgs.libvirt}/bin/virsh list --name | grep -q "^hass$"; then
-          ${pkgs.libvirt}/bin/virsh detach-device hass /etc/homeassistant/conbee.xml
+          ${pkgs.libvirt}/bin/virsh detach-device hass /etc/homeassistant/thread.xml
           ${pkgs.libvirt}/bin/virsh detach-device hass /etc/homeassistant/zbt2.xml
           ${pkgs.coreutils}/bin/sleep 5
-          ${pkgs.libvirt}/bin/virsh attach-device hass /etc/homeassistant/conbee.xml
+          ${pkgs.libvirt}/bin/virsh attach-device hass /etc/homeassistant/thread.xml
           ${pkgs.libvirt}/bin/virsh attach-device hass /etc/homeassistant/zbt2.xml
         fi;
       '';
