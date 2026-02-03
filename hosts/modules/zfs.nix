dd8f6fbdebf6fcfd6317466f773bdb2c858e324c
Scott Hansen <tech@firecat53.net>
2026-02-03 10:33:04 -0800

    Use unstable zfs packages and kernel when latestZFSVersion is enabled for more recent packages

--- hosts/modules/zfs.nix
+++ hosts/modules/zfs.nix
@@ -6,13 +6,17 @@
   ...
 }:
 let
+  # Use unstable packages when latestZFSKernel is enabled for better kernel support
+  zfsPkgs = if config.latestZFSKernel then pkgs.unstable else pkgs;
+  zfsPackage = if config.latestZFSKernel then zfsPkgs.zfs_unstable else pkgs.zfs;
+
   # Find latest available NixOS kernel compatible with ZFS
   zfsCompatibleKernelPackages = lib.filterAttrs (
     name: kernelPackages:
     (builtins.match "linux_[0-9]+_[0-9]+" name) != null
     && (builtins.tryEval kernelPackages).success
-    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
-  ) pkgs.linuxKernel.packages;
+    && (!kernelPackages.${zfsPackage.kernelModuleAttribute}.meta.broken)
+  ) zfsPkgs.linuxKernel.packages;
   latestKernelPackage = lib.last (
     lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
       builtins.attrValues zfsCompatibleKernelPackages
@@ -20,6 +24,9 @@ let
   );
 in
 {
+  # Use zfs_unstable when latestZFSKernel is enabled
+  boot.zfs.package = lib.mkIf config.latestZFSKernel zfsPackage;
+
   # Note this might jump back and forth as kernels are added or removed.
   boot.kernelPackages = lib.mkIf config.latestZFSKernel latestKernelPackage;
 
