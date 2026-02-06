4d7e32f6380af7f0930c4da0053f26d796b16feb
Scott Hansen <tech@firecat53.net>
2026-02-06 13:24:07 -0800

    Update pangolin to 1.15.2

--- hosts/pangolin/services/pangolin.nix
+++ hosts/pangolin/services/pangolin.nix
@@ -9,7 +9,7 @@ let
   pangolinHost = "pangolin.${baseDomain}";
 
   # Docker images
-  pangolinImage = "fosrl/pangolin:1.15.1";
+  pangolinImage = "fosrl/pangolin:1.15.2";
   gerbilImage = "fosrl/gerbil:1.3.0";
 
   # Ports
