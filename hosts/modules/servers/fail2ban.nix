0d3dcbb67dfa1ed1fc62b33179584857a2b81baf
Scott Hansen <tech@firecat53.net>
2026-04-02 09:06:55 -0700

    Add portscan jail to fail2ban

--- hosts/modules/servers/fail2ban.nix
+++ hosts/modules/servers/fail2ban.nix
@@ -1,4 +1,24 @@
 {
   ### Fail2ban
-  services.fail2ban.enable = true;
+  services.fail2ban = {
+    enable = true;
+    bantime = "1h";
+    jails = {
+      portscan = {
+        settings = {
+          enabled = true;
+          filter = "portscan";
+          backend = "systemd";
+          maxretry = 5;
+          findtime = 60;
+          bantime = 3600;
+          action = "iptables-allports[name=portscan, protocol=all]";
+        };
+      };
+    };
+  };
+  environment.etc."fail2ban/filter.d/portscan.conf".text = ''
+    [Definition]
+    failregex = refused connection: .* SRC=<HOST>
+  '';
 }
