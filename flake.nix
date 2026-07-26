2631bf0221d33783dc98416f54fce33288d9730e
Scott Hansen <tech@firecat53.net>
2026-07-26 16:53:30 -0700

    Change sops-nixs to follow stable instead of unstable (per sops-nix docs)

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     disko.inputs.nixpkgs.follows = "nixpkgs";
 
     sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
